5937145d9a43253c60896ec8daa75d43378b4663
JonathanSamuelTan <jonathansam215@gmail.com>
2024-08-18 12:42:37 +0700

    done

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,31 +4,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv',delimiter=',')
 
 # 2
-df['overweight'] = None
+df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2)).apply(lambda x: 1 if x > 25 else 0)
 
 # 3
-
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
-
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
+    
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['variable', 'value', 'cardio']).size().reset_index(name='total')
     
 
     # 7
-
-
-
+    catplot = sns.catplot(
+        x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar'
+    )
+    
     # 8
-    fig = None
-
+    fig = catplot.fig
 
     # 9
     fig.savefig('catplot.png')
@@ -38,22 +38,32 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) & 
+        (df['height'] >= df['height'].quantile(0.025)) & 
+        (df['height'] <= df['height'].quantile(0.975)) & 
+        (df['weight'] >= df['weight'].quantile(0.025)) & 
+        (df['weight'] <= df['weight'].quantile(0.975))
+        ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 12))
 
-    # 15
+    # 15 
+    heatmap = sns.heatmap(
+        corr, mask=mask, annot=True, fmt='.1f', cmap='coolwarm',
+        center=0, square=True, cbar_kws={'shrink': 0.5}, ax=ax
+    )
 
-
+    fig = heatmap.get_figure()
 
     # 16
     fig.savefig('heatmap.png')
